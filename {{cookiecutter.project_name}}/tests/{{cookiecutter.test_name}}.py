e48a04b1835d51825c8b933cd08208889e3a8c1a
Prakhar Pratyush <er.prakhar2b@gmail.com>
2019-05-18 09:18:17 +0530

    Update {{cookiecutter.test_name}}.py

--- {{cookiecutter.project_name}}/tests/{{cookiecutter.test_name}}.py
+++ {{cookiecutter.project_name}}/tests/{{cookiecutter.test_name}}.py
@@ -8,12 +8,21 @@
 import pytest
 from tests.helpers import do_steps
 
+class Objectify(object):
+	def __init__(self, d):
+		for a, b in d.items():
+			if isinstance(b, (list, tuple)):
+			   setattr(self, a, [Objectify(x) if isinstance(x, dict) else x for x in b])
+			else:
+			   setattr(self, a, Objectify(b) if isinstance(b, dict) else b)
+			
 test_config = {{cookiecutter.test_config}}
+test_config = Objectify(test_config)
 
-def test_google(driver_):
-	base_url = test_config['base_url']
-	global_step_wait = test_config['global_step_wait']
-	steps = test_config['steps']
+def {{cookiecutter.test_name}}(driver_):
+	ase_url = test_config.base_url
+	global_step_wait = test_config.global_step_wait
+	steps = test_config.steps
 	
 	global driver 
 	driver = driver_
